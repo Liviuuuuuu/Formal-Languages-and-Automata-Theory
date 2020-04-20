520aea9898ee4889da48bf8a200fb5ecb65a06ac
Liviu Bouruc <bouruc.liviu2000@gmail.com>
2020-04-20 17:21:05 +0300

    Add files via upload

--- Proiect_2/main.cpp
+++ Proiect_2/main.cpp
@@ -3,10 +3,13 @@
 #include <vector>
 #include <map>
 #include <set>
+#include <queue>
 
 using namespace std;
 
 ifstream fin("date.in");
+
+set <char> alf;
 
 class NFA
 {
@@ -16,12 +19,13 @@ class NFA
 public:
     void removeLambda()
     {
-        bool ok = 0;
+        bool ok = 1;
         while(ok)
         {
-            ok = 1;
+            ok = 0;
             for(int i = 1; i <= nrStari; ++i)
             {
+                if(!tranzitii[i].count('$')) continue;
                 for(auto it : tranzitii[i]['$'])
                 {
                     for(auto it2 : tranzitii[it])
@@ -36,10 +40,13 @@ public:
             }
         }
     }
+    int getNrStari() const { return nrStari; }
+    int getSI() const { return si; }
+    vector <map <char, set<int> > > getTranz() const { return tranzitii; }
+    vector <bool> getF() const { return f; }
     friend istream& operator>>(istream&, NFA&);
     friend ostream& operator<<(ostream&, const NFA&);
 };
-
 istream& operator>>(istream& in, NFA& a)
 {
     int aux;
@@ -52,6 +59,7 @@ istream& operator>>(istream& in, NFA& a)
         char c;
         in >> x >> y >> c;
         a.tranzitii[x][c].insert(y);
+        if(c != '$') alf.insert(c);
     }
     in >> a.si >> aux;
     for(int i = 1, x; i <= aux; ++i)
@@ -72,16 +80,96 @@ ostream& operator<<(ostream& out, const NFA& a)
     for(int i = 1; i <= a.nrStari; ++i)
         for(auto it : a.tranzitii[i])
             for(auto j : it.second) out << i << " " << j << " " << it.first << '\n';
+
+    return out;
+}
+
+class DFA
+{
+    int nrStari, si;
+    vector <map <char, int> > tranzitii;
+    vector <bool> f;
+public:
+    DFA(const NFA& anfa)
+    {
+        nrStari = 0;
+        map <set <int>, int> myMap;
+        vector <map <char, set<int> > > nfaTranz = anfa.getTranz();
+        vector <bool> nfaF = anfa.getF();
+        f.push_back(0); // incepem de pe 1
+        queue <set <int> > Q;
+        set <int> aux;
+
+        si = anfa.getSI();
+        aux.insert(si);
+        f.push_back(nfaF[si]);
+        myMap[aux] = ++nrStari;
+        tranzitii.push_back(map <char, int>());
+
+        Q.push(aux);
+        while(!Q.empty())
+        {
+            set <int> nodes = Q.front();
+            Q.pop();
+
+            for(char c : alf)
+            {
+                aux.clear();
+                for(auto it : nodes)
+                {
+                    if(!nfaTranz[it].count(c)) continue;
+                    for(auto to : nfaTranz[it][c]) aux.insert(to);
+                }
+                if(!aux.size()) continue;
+                if(!myMap.count(aux))
+                {
+                    myMap[aux] = ++nrStari;
+                    bool Final = 0;
+                    for(auto it : aux)
+                        if(nfaF[it]) Final = 1;
+                    f.push_back(Final);
+                    tranzitii.push_back(map <char, int>());
+                    Q.push(aux);
+                }
+                tranzitii[myMap[nodes]][c] = myMap[aux];
+            }
+        }
+    }
+    friend ostream& operator<<(ostream&, const DFA&);
+};
+ostream& operator<<(ostream& out, const DFA& a)
+{
+    out << a.nrStari << " stari\n";
+    out << "Starea initiala: " << a.si << '\n';
+    out << "Stari finale: ";
+    for(int i = 1; i <= a.nrStari; ++i)
+        if(a.f[i]) out << i << " ";
+    out << '\n';
+    for(int i = 1; i <= a.nrStari; ++i)
+    {
+        if(a.tranzitii[i].empty()) continue;
+        for(auto it : a.tranzitii[i]) out << i << " " << it.second << " " << it.first << '\n';
+    }
+    return out;
 }
 
 int main()
 {
-    NFA automat;
-    fin >> automat;
+    NFA anfa;
+    fin >> anfa;
     cout << "Automatul initial:\n";
-    cout << automat;
-    automat.removeLambda();
+    cout << anfa;
+
+    anfa.removeLambda();
     cout << "\nAm transformat automatul din Lambda-NFA in NFA:\n";
-    cout << automat;
+    cout << anfa;
+
+    DFA adfa(anfa);
+    cout << "\nAm transformat NFA-ul in DFA:\n";
+    cout << adfa;
+
+    //adfa.minimisation();
+    cout << "\nAm minimizat DFA-ul:\n";
+    //cout << adfa;
     return 0;
 }
